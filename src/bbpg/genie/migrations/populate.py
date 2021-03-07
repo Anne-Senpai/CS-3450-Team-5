f6e18ee0b3c26a769c53a1d1b08d95ac2ad446f4
Derek Ward <derek.ward@ep-sys.net>
2021-03-07 14:47:00 -0700

    testing code with me

--- src/bbpg/genie/migrations/populate.py
+++ src/bbpg/genie/migrations/populate.py
@@ -2,4 +2,5 @@ from django.db import migrations
 from django.utils import timezone
 
 def populate_db(apps, schema_editor):
-    Event = apps.get_model
+    Event = apps.get_model("genie", "Event")
+    
--- src/bbpg/genie/views.py
+++ src/bbpg/genie/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from genie.models import Event
+import datetime
+
 def sample(request):
     return render(request, 'genie/sample.html')
 
@@ -17,4 +20,6 @@ def register(request):
 
     return render(request, "genie/register.html", {"form": form})
 
-
+def events(request):
+    events = Event.objects.get(startTime__gt=datetime.datetime.now())
+    return render(request, "genie/events.html", {"events": events})
