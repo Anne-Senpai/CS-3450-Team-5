4390a9f37522b9a2833c0437fc99969675ff6fd4
makaleebeelek <77854628+makaleebeelek@users.noreply.github.com>
2021-04-13 20:06:38 -0600

    Test

--- src/bbpg/genie/tests.py
+++ src/bbpg/genie/tests.py
@@ -1,3 +1,62 @@
 from django.test import TestCase
+from genie.models import Event, Reservation, ParkingLot, LotArea, Profile
 
-# Create your tests here.
+class unittest(TestCase):
+
+    def testLot(self):
+        parkingLot = ParkingLot()
+        parkingLot.owner = "Arthur"
+        parkingLot.name = "Arthur's Kingdom"
+        parkingLot.address = "123 Arthur Street"
+
+        self.assertTrue(parkingLot != None)
+        self.assertEqual(parkingLot.owner, "Arthur")
+        self.assertEqual(parkingLot.name, "Arthur's Kingdom")
+        self.assertEqual(parkingLot.address, "123 Arthur Street")
+
+
+    def testEvent(self):
+        parkingLot = ParkingLot()
+        parkingLot.owner = "Arthur"
+        parkingLot.name = "Arthur's Kingdom"
+        parkingLot.address = "123 Arthur Street"
+
+        event = Event()
+        event.startTime = "1:00 p.m."
+        event.endTime = "1:01 p.m."
+        event.name = "Tuna Fish"
+        event.description = "Eat Yummy Dinner!!"
+        event.address = "123 Arthur Street"
+        event.parkingLots = parkingLot
+
+        self.assertTrue(event != None)
+        self.assertEqual(parkingLot.startTime, "1:00 p.m.")
+        self.assertEqual(parkingLot.endTime, "1:01 p.m.")
+        self.assertEqual(parkingLot.name, "Tuna Fish")
+        self.assertEqual(parkingLot.description, "Eat Yummy Dinner!!")
+        self.assertEqual(parkingLot.address, "123 Arthur Street")
+        self.assertEqual(parkingLot.parkingLots, parkingLot)
+
+    def testLotArea(self):
+        parkingLot = ParkingLot()
+        parkingLot.owner = "Arthur"
+        parkingLot.name = "Arthur's Kingdom"
+        parkingLot.address = "123 Arthur Street"
+
+        lotArea = LotArea()
+        lotArea.areaIdentifier = "Area 51"
+        lotArea.parkingLot = parkingLot
+        lotArea.price = "Your left kidney"
+        lotArea.type = "Regular"
+        lotArea.capacity = "1"
+
+        self.assertTrue(lotArea != None)
+        self.assertEqual(lotArea.areaIdentifier, "Area 51")
+        self.assertEqual(lotArea.parkingLot, parkingLot)
+        self.assertEqual(lotArea.price, "Your left kidney")
+        self.assertEqual(lotArea.type, "Regular")
+        self.assertEqual(lotArea.capacity, "1")
+
+
+
+
